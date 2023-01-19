83ac3417c463f37223d0caddd31b85437e46ffe0
Sara Szwejkowska <saraszw@op.pl>
2023-01-19 12:26:09 +0100

    new changes

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-const next = document.querySelector(".nextStep");
-const back = document.querySelector(".backStep");
-const input = document.querySelectorAll("#emailAdress");
-const select = document.querySelector(".select");
-const wrapper = document.querySelector(".wrapper");
+const next = document.querySelector('.nextStep');
+const back = document.querySelector('.backStep');
+const input = document.querySelector('#emailAdress');
+const select = document.querySelector('.select');
+const wrapper = document.querySelector('.wrapper');
+const customSwitch=document.querySelector("#switch")
 
-next.addEventListener('click', () =>{
-if(input === '') {
-    alert('bug');
-
-}else{
-select.classList.remove('none');
-wrapper.style.display ='none';
-}
-
+next.addEventListener('click', () => {
+	// if (input.value === '') {
+	// 	//alert('bug');
+	// } else {
+	select.style.display = 'block';
+	wrapper.style.display = 'none';
+    customSwitch.style.display='inline'
+	//}
 });

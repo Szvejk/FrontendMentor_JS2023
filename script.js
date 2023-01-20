0d105542e5e073fcd9d3daffb57b891a4bd72175
Sara Szwejkowska <saraszw@op.pl>
2023-01-20 15:40:34 +0100

    crating 3 view

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ const back = document.querySelector('.backStep');
 const input = document.querySelector('#emailAdress');
 const select = document.querySelector('.select');
 const wrapper = document.querySelector('.wrapper');
-const customSwitch=document.querySelector("#switch")
+const secondView = document.querySelector('.secondView');
+const pick = document.querySelector('.pickAdd-ons');
 
+pick.style.display = 'none';
 next.addEventListener('click', () => {
 	// if (input.value === '') {
 	// 	//alert('bug');
 	// } else {
 	select.style.display = 'block';
 	wrapper.style.display = 'none';
-    customSwitch.style.display='inline'
+
+	next.addEventListener('click', () => {
+		select.style.display = 'none';
+		pick.style.display = 'block';
+	});
+
 	//}
 });

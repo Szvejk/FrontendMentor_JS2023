8322476ebe519503efcea58cb2652d857f8bebdf
Sara Szwejkowska <saraszw@op.pl>
2023-01-26 13:24:41 +0100

    created a summary function

--- script.js
+++ script.js
@@ -27,20 +27,30 @@ const prizeOfServices2 = document.querySelector('.prizeSecond');
 const prizeOfServices3 = document.querySelector('.prizeThird');
 const largerStorage = document.querySelector('.largerStorage');
 const customizable = document.querySelector('.customizable');
-
+const summaryTotal = document.querySelector('.summaryTotal');
 let yearlyOrMonthly = 'monthly';
+const numberOne = document.querySelector('.numberOne');
+const numberSecond = document.querySelector('.numberSecond');
+const numberThree = document.querySelector('.numberThree');
+const numberFour = document.querySelector('.numberFour');
+
+let sumOrder = 0;
 
 // step 2
 arcade.addEventListener('click', () => {
 	summary.innerHTML = `Arcade<div class="numberSummary">${prizeFirst.textContent}</div>`;
+	sumOrder += 9;
+	console.log(sumOrder);
 	arcade.style.borderColor = 'darkblue';
 });
 advanced.addEventListener('click', () => {
+	sumOrder += 12;
 	summary.innerHTML = `Advanced<div class="numberSummary">${prizeSecond.textContent}</div>`;
 	selectedPlan = 'Advanced';
 	advanced.style.borderColor = 'darkblue';
 });
 pro.addEventListener('click', () => {
+	sumOrder += 15;
 	summary.innerHTML = `Pro<div class="numberSummary">${prizeThird.textContent}</div>`;
 	selectedPlan = 'pro';
 	pro.style.borderColor = 'darkblue';
@@ -55,32 +65,39 @@ switcher.addEventListener('click', () => {
 	total.innerHTML = `Total(per ${yearlyOrMonthly})`;
 });
 
+// summaryTotal.innerHTML = `<div class="totalNumber">${result.textContent}</div>`;
+
 // step 3
 
 option1.addEventListener('click', (e) => {
 	if (e.target !== inputOption1) {
+		console.log(sumOrder);
+		sumOrder += 1;
 		inputOption1.checked = !inputOption1.checked;
+		service.style.display = 'flex';
 	}
-	service.innerHTML = `Online service<div class="numberService"> ${prizeOfServices1.textContent}</div> `;
+	service.innerHTML = `Online service<div class="numberServiceS"> ${prizeOfServices1.textContent}</div> `;
 });
 
 option2.addEventListener('click', (e) => {
+	sumOrder += 2;
 	if (e.target !== inputOption2) {
 		inputOption2.checked = !inputOption2.checked;
+		largerStorage.style.display = 'flex';
 	}
 	largerStorage.innerHTML = `Larger storage<div class="numberService2"> ${prizeOfServices2.textContent}</div> `;
 });
 
 option3.addEventListener('click', (e) => {
+	sumOrder += 2;
 	if (e.target !== inputOption3) {
 		inputOption3.checked = !inputOption3.checked;
+		customizable.style.display = 'flex';
 	}
+	customizable.innerHTML = `Customizable<div class="numberService3"> ${prizeOfServices3.textContent}</div> `;
 });
-if ((inputOption3.checked = true)) {
-	customizable.innerHTML = `Customizable<div class="numberService3"> ${prizeOfServices2.textContent}</div> `;
-} else {
-	customizable.style.display = 'none';
-}
+
+// total
 
 let counter = 0;
 
@@ -92,6 +109,10 @@ const checkView = (value) => {
 		view2.style.display = 'none';
 		view3.style.display = 'none';
 		next.style.display = 'block';
+		numberOne.style.backgroundColor = 'blue';
+		numberSecond.style.backgroundColor = 'transparent';
+		numberThree.style.backgroundColor = 'transparent';
+		numberFour.style.backgroundColor = 'transparent';
 	} else if (value == 1) {
 		back.style.display = 'block';
 		view0.style.display = 'none';
@@ -99,6 +120,8 @@ const checkView = (value) => {
 		view2.style.display = 'none';
 		view3.style.display = 'none';
 		next.style.display = 'block';
+		numberOne.style.backgroundColor = 'transparent';
+		numberSecond.style.backgroundColor = 'blue';
 	} else if (value == 2) {
 		back.style.display = 'block';
 		view0.style.display = 'none';
@@ -106,6 +129,9 @@ const checkView = (value) => {
 		view2.style.display = 'flex';
 		view3.style.display = 'none';
 		next.style.display = 'block';
+		numberOne.style.backgroundColor = 'transparent';
+		numberSecond.style.backgroundColor = 'transparent';
+		numberThree.style.backgroundColor = 'blue';
 	} else if (value == 3) {
 		back.style.display = 'block';
 		view0.style.display = 'none';
@@ -113,6 +139,12 @@ const checkView = (value) => {
 		view2.style.display = 'none';
 		view3.style.display = 'flex';
 		next.style.display = 'none';
+		numberFour.style.backgroundColor = 'blue';
+		numberOne.style.backgroundColor = 'transparent';
+		numberSecond.style.backgroundColor = 'transparent';
+		numberThree.style.backgroundColor = 'transparent';
+
+		summaryTotal.innerHTML = sumOrder;
 	}
 };
 checkView(0);
